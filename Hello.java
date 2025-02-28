2afba0b78257b0a2b633630de360124c611a7266
Pedro-Pires-fag <pebpires@minha.fag.edu.br>
2025-02-27 21:16:47 -0300

    primeiro commit

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        printHello();
+        printOlaMundo();
+    }
+
+    public static void printHello()
+    {
+        System.out.println("Hello World!");
+    }
+
+    public static void printOlaMundo()
+    {
+        System.out.println("Ola Mundo!");
+    }
+}
+
+
